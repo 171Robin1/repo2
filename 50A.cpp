f924edf60ba9b6e837f17f069448dd261001a96d
Marina <133687575+171Robin1@users.noreply.github.com>
2023-05-15 20:01:37 +0300

    Add files via upload

---
+++ 50A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+#define vll vector<long long>
+
+#define start ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#define range int t; cin >> t; while(t--)
+
+using namespace std;
+
+int n, k, q;
+int a[51];
+
+int main()
+{
+    start
+    cin >> n >> k;
+
+    cout << n*k/2;
+}
+
+
+
